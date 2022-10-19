3b00ecaa4bf1b6116a1e0bdb16570f449579df16
Jules! <69649546+Dayucoffee@users.noreply.github.com>
2022-10-19 18:58:16 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv 
+
+load_dotenv()
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Configuration(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'todo.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
